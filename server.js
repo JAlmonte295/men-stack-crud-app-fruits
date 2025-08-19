217901439d8488c649139449f19bd1e13e4deaf6
jalmontegit <jalmonte721@gmail.com>
2025-08-18 20:53:56 -0400

    lab, still gotta finish Edit and the rest of CRUD

---
+++ server.js
@@ -0,0 +1,78 @@
+// Here is where we import modules
+
+// We begin by loading Express
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+
+// Import the Fruit model
+const Fruit = require("./models/fruit.js");
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+
+// Routes
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
+
+// GET /
+app.get("/", async (req, res) => {
+  res.render("startup.ejs");
+});
+
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+console.log(allFruits); // log the fruits!
+//   res.send("Welcome to the index page!");
+res.render("fruits/index.ejs", { fruits: allFruits });
+});
+
+
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+
+// POST /fruits // Defining ROUTE
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+  res.redirect("/fruits");
+});
+
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
+});
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+
+
