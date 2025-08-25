475f1878eb0576d1d430eef8f4f2baafd0436ca1
jalmontegit <jalmonte721@gmail.com>
2025-08-25 18:43:44 -0400

    style added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 const morgan = require('morgan');
 const methodOverride = require('method-override');
-
+const path = require("path");
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -25,6 +25,8 @@ const Fruit = require("./models/fruit.js");
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // Routes
 
@@ -68,6 +70,35 @@ app.get("/fruits/:fruitId", async (req, res) => {
   const foundFruit = await Fruit.findById(req.params.fruitId);
   res.render("fruits/show.ejs", { fruit: foundFruit });
 });
+
+// PATCH / EDIT
+
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render('fruits/edit.ejs', {
+    fruit: foundFruit,
+  })
+});
+
+// UPDATE
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
+
+// DELETE
 
 app.delete("/fruits/:fruitId", async (req, res) => {
   await Fruit.findByIdAndDelete(req.params.fruitId);
